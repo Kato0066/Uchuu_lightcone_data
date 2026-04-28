80c5b679142435ad387a2b1148a0c6a7c8c8b12c
Kato0066 <amiami200034@gmail.com>
2026-04-28 10:34:38 +0900

    replace latest 4/23

--- src/make_lightcone.c
+++ src/make_lightcone.c
@@ -42,28 +42,6 @@ typedef struct GadgetHeader {
 } GadgetHeader;
 
 
-static int read_block_header(FILE *fin, int *blksize)
-{
-  if (fread(blksize, sizeof(int), 1, fin) != 1) {
-    return -1;
-  }
-  return 0;
-}
-
-static int read_block_footer(FILE *fin, int expected_blksize)
-{
-  int blksize;
-
-  if (fread(&blksize, sizeof(int), 1, fin) != 1) {
-    return -1;
-  }
-  if (blksize != expected_blksize) {
-    fprintf(stderr, "block size mismatch: %d != %d\n", blksize, expected_blksize);
-    return -1;
-  }
-  return 0;
-}
-
 int get_gadget_npart(const char *filename)
 {
   FILE *fin;
@@ -76,20 +54,11 @@ int get_gadget_npart(const char *filename)
     return -1;
   }
 
-  if (read_block_header(fin, &blksize) != 0) {
-    fclose(fin);
-    return -1;
-  }
-
-  if (fread(&gadget_header, sizeof(GadgetHeader), 1, fin) != 1) {
-    fclose(fin);
-    return -1;
-  }
-
-  if (read_block_footer(fin, blksize) != 0) {
-    fclose(fin);
-    return -1;
-  }
+  fread( &blksize, 4, 1, fin);
+  //  fprintf( stderr, "Reading Gadget file blksize= %d\n", blksize);
+  fread( &gadget_header, sizeof(gadget_header), 1, fin);
+  fread( &blksize, 4, 1, fin);
+  //  fprintf( stderr, "End Reading Gadget file blksize= %d\n", blksize);
 
   fclose(fin);
 
@@ -105,67 +74,37 @@ int read_gadget_ptcl(const char *filename, Particle *ptcl)
   GadgetHeader gadget_header;
   float *cache;
 
+  int32_t nmemory = IO_CACHE_SIZE;
+
   fin = fopen(filename, "rb");
   if (fin == NULL) {
     fprintf(stderr, "cannot open %s\n", filename);
     return -1;
   }
 
-  if (read_block_header(fin, &blksize) != 0) {
-    fclose(fin);
-    return -1;
-  }
-  if (fread(&gadget_header, sizeof(GadgetHeader), 1, fin) != 1) {
-    fclose(fin);
-    return -1;
-  }
-  if (read_block_footer(fin, blksize) != 0) {
-    fclose(fin);
-    return -1;
-  }
+  //  fprintf( stderr, "Reading Gadget file blksize= %d\n", blksize);
+  fread( &blksize, 4, 1, fin);
+  fread( &gadget_header, sizeof(gadget_header), 1, fin);
+  fread( &blksize, 4, 1, fin);
+  //  fprintf( stderr, "End Reading Gadget file blksize= %d\n", blksize);
 
   npart = gadget_header.Npart[1];
 
   cache = (float *)malloc(sizeof(float) * 3 * IO_CACHE_SIZE);
-  if (cache == NULL) {
-    fprintf(stderr, "malloc failed for cache\n");
-    fclose(fin);
-    return -1;
-  }
-
-  if (read_block_header(fin, &blksize) != 0) {
-    free(cache);
-    fclose(fin);
-    return -1;
-  }
-
-  for (i = 0; i < npart; i += IO_CACHE_SIZE) {
-    int nread = IO_CACHE_SIZE;
-    int j;
-
-    if (i + nread > npart) {
-      nread = npart - i;
-    }
-
-    if (fread(cache, sizeof(float), 3 * nread, fin) != (size_t)(3 * nread)) {
-      fprintf(stderr, "failed to read positions from %s\n", filename);
-      free(cache);
-      fclose(fin);
-      return -1;
-    }
-
-    for (j = 0; j < nread; j++) {
-      ptcl[i + j].r[0] = cache[3 * j + 0];
-      ptcl[i + j].r[1] = cache[3 * j + 1];
-      ptcl[i + j].r[2] = cache[3 * j + 2];
+  fread( &blksize, 4, 1, fin);
+  //  fprintf( stderr, "Reading Gadget file blksize= %d\n", blksize);
+  for( int i=0; i<npart; i+=nmemory){
+    int nread = nmemory;
+    if( (i+nmemory) > npart)  nread = npart - i;
+    fread( cache, sizeof(float), 3*nread, fin);
+    for( int j=0; j<nread; j++){
+      ptcl[i+j].r[0] = cache[3*j+0];
+      ptcl[i+j].r[1] = cache[3*j+1];
+      ptcl[i+j].r[2] = cache[3*j+2];
     }
   }
-
-  if (read_block_footer(fin, blksize) != 0) {
-    free(cache);
-    fclose(fin);
-    return -1;
-  }
+  fread( &blksize, 4, 1, fin);
+  //  fprintf( stderr, "End Reading Gadget file blksize= %d\n", blksize);
 
   free(cache);
   fclose(fin);
@@ -179,8 +118,7 @@ int main(int argc, char **argv)
   FILE *fout;
   Particle *ptcl;
   char filename[256];
-  int32_t i;
-  long long total_selected = 0;
+  int64_t total_selected = 0;
 
   if (argc != 2) {
     fprintf(stderr, "Usage: %s base_filename\n", argv[0]);
@@ -193,15 +131,18 @@ int main(int argc, char **argv)
     return 1;
   }
 
+  int64_t npart_total = 0;
 
-  for (i = 0; i < 200; i++) {
+  for (int32_t i = 0; i < 200; i++) {
     int32_t npart;
-    int32_t j;
-    int32_t nsel = 0;
+
+    fprintf(stdout, "# Reading %d-th file out of 200.\n", i);
 
     sprintf(filename, "%s.%d", argv[1], i);
 
     npart = get_gadget_npart(filename);
+
+    npart_total += npart;
     if (npart < 0) break;
 
     ptcl = (Particle *) malloc(sizeof(Particle)*npart);
@@ -211,24 +152,13 @@ int main(int argc, char **argv)
       return 1;
     }
 
-    if (read_gadget_ptcl(filename, ptcl) < 0) {
-      free(ptcl);
-      fclose(fout);
-      return 1;
-    }
+    read_gadget_ptcl(filename, ptcl);
 
-    /*printf("%s : npart = %d\n", filename, npart);
-      if (npart > 0) {
-      printf("  ptcl[0] r = (%g, %g, %g)\n",
-      ptcl[0].r[0],
-      ptcl[0].r[1],
-      ptcl[0].r[2]);
-      }
-    */
-
-    for (j = 0; j < npart; j++) {
+    int32_t nsel = 0;
+#pragma omp parallel for schedule(auto) reduction(+:nsel)
+    for (int32_t j = 0; j < npart; j++) {
       if (ptcl[j].r[2] >= ZMIN && ptcl[j].r[2] <= ZMAX) {
-        fprintf(fout, "%.10g %.10g %.10g\n",
+        fprintf(fout, "%14.6e %14.6e %14.6e\n",
                 ptcl[j].r[0],
                 ptcl[j].r[1],
                 ptcl[j].r[2]);
@@ -237,14 +167,15 @@ int main(int argc, char **argv)
     }
 
     total_selected += nsel;
-    printf("  selected in %.1f <= z <= %.1f : %d\n", ZMIN, ZMAX, nsel);
+    printf("#  selected in %12.4e <= z <= %12.4e : %d\n", ZMIN, ZMAX, nsel);
 
     free(ptcl);
   }
 
   fclose(fout);
 
-  printf("total selected particles = %lld\n", total_selected);
+  printf("# total number of particle in this snapshot : %ld\n", npart_total);
+  printf("# total selected particles = %lld\n", total_selected);
   printf("wrote zslice_xy.dat\n");
 
   return 0;
