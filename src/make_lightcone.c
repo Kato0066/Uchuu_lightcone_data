37e1b17a2a3155d759c509e4f0732aec07eae436
Kato0066 <amiami200034@gmail.com>
2026-04-30 07:45:46 +0900

    update

--- src/make_lightcone.c
+++ src/make_lightcone.c
@@ -11,8 +11,11 @@ const double Gadget_UnitMass_in_Msun = 1.0e10;          // 1e10 Msun
 const double Gadget_UnitVelocity_in_cm_per_s = 1e5;     //  1 km/sec
 
 #define IO_CACHE_SIZE 2097152
-#define ZMIN 0.0
-#define ZMAX 0.1
+#define NGRID 64
+#define RHO(a,b,c) mesh_density[(c) + ngrid * ((b) + ngrid * (a))]
+#define BOXSIZE 2000.0
+//#define ZMIN 0.0
+//#define ZMAX 0.1
 
 
 typedef struct Particle{
@@ -40,6 +43,7 @@ typedef struct GadgetHeader {
   int      flag_entr_ics;
   char     unused[256 - 6*4 - 6*8 - 2*8 - 2*4 - 6*4 - 2*4 - 4*8 - 9*4];
 } GadgetHeader;
+
 
 
 int get_gadget_npart(const char *filename)
@@ -90,6 +94,10 @@ int read_gadget_ptcl(const char *filename, Particle *ptcl)
 
   npart = gadget_header.Npart[1];
 
+  for (i = 0; i < npart; i++) {
+    ptcl[i].mass = gadget_header.Massarr[1];
+  }
+
   cache = (float *)malloc(sizeof(float) * 3 * IO_CACHE_SIZE);
   fread( &blksize, 4, 1, fin);
   //  fprintf( stderr, "Reading Gadget file blksize= %d\n", blksize);
@@ -111,6 +119,112 @@ int read_gadget_ptcl(const char *filename, Particle *ptcl)
   return npart;
 }
 
+void calc_mesh_density(double *mesh_density, Particle *ptcl,
+                       int npart, int ngrid, double boxsize)
+{
+  int p;
+
+  for (int i = 0; i < ngrid; i++) {
+    for (int j = 0; j < ngrid; j++) {
+      for (int k = 0; k < ngrid; k++) {
+        RHO(i, j, k) = 0.e0;
+      }
+    }
+  }
+
+  for (p = 0; p < npart; p++) {
+    double xt1;
+    double dx1;
+    double wi11, wi21, wi31, wi12, wi22, wi32, wi13, wi23, wi33;
+    int iw11, iw21, iw31, iw12, iw22, iw32, iw13, iw23, iw33;
+
+    xt1  = (double)ptcl[p].r[0] / boxsize * (double)ngrid;
+    iw21 = (int)(xt1 + 0.5);
+    dx1  = xt1 - (double)iw21;
+    wi11 = 0.5 * (0.5 - dx1) * (0.5 - dx1);
+    wi21 = 0.75 - dx1 * dx1;
+    wi31 = 0.5 * (0.5 + dx1) * (0.5 + dx1);
+    iw11 = iw21 - 1;
+    iw31 = iw21 + 1;
+    if (iw21 == 0) {
+      iw11 = ngrid - 1;
+    } else if (iw21 == ngrid - 1) {
+      iw31 = 0;
+    } else if (iw21 == ngrid) {
+      iw21 = 0;
+      iw31 = 1;
+    }
+
+    xt1  = (double)ptcl[p].r[1] / boxsize * (double)ngrid;
+    iw22 = (int)(xt1 + 0.5);
+    dx1  = xt1 - (double)iw22;
+    wi12 = 0.5 * (0.5 - dx1) * (0.5 - dx1);
+    wi22 = 0.75 - dx1 * dx1;
+    wi32 = 0.5 * (0.5 + dx1) * (0.5 + dx1);
+    iw12 = iw22 - 1;
+    iw32 = iw22 + 1;
+    if (iw22 == 0) {
+      iw12 = ngrid - 1;
+    } else if (iw22 == ngrid - 1) {
+      iw32 = 0;
+    } else if (iw22 == ngrid) {
+      iw22 = 0;
+      iw32 = 1;
+    }
+
+    xt1  = (double)ptcl[p].r[2] / boxsize * (double)ngrid;
+    iw23 = (int)(xt1 + 0.5);
+    dx1  = xt1 - (double)iw23;
+    wi13 = 0.5 * (0.5 - dx1) * (0.5 - dx1);
+    wi23 = 0.75 - dx1 * dx1;
+    wi33 = 0.5 * (0.5 + dx1) * (0.5 + dx1);
+    iw13 = iw23 - 1;
+    iw33 = iw23 + 1;
+    if (iw23 == 0) {
+      iw13 = ngrid - 1;
+    } else if (iw23 == ngrid - 1) {
+      iw33 = 0;
+    } else if (iw23 == ngrid) {
+      iw23 = 0;
+      iw33 = 1;
+    }
+
+    wi11 *= ptcl[p].mass;
+    wi21 *= ptcl[p].mass;
+    wi31 *= ptcl[p].mass;
+
+    RHO(iw11,iw12,iw13)=RHO(iw11,iw12,iw13)+wi11*wi12*wi13;
+    RHO(iw21,iw12,iw13)=RHO(iw21,iw12,iw13)+wi21*wi12*wi13;
+    RHO(iw31,iw12,iw13)=RHO(iw31,iw12,iw13)+wi31*wi12*wi13;
+    RHO(iw11,iw22,iw13)=RHO(iw11,iw22,iw13)+wi11*wi22*wi13;
+    RHO(iw21,iw22,iw13)=RHO(iw21,iw22,iw13)+wi21*wi22*wi13;
+    RHO(iw31,iw22,iw13)=RHO(iw31,iw22,iw13)+wi31*wi22*wi13;
+    RHO(iw11,iw32,iw13)=RHO(iw11,iw32,iw13)+wi11*wi32*wi13;
+    RHO(iw21,iw32,iw13)=RHO(iw21,iw32,iw13)+wi21*wi32*wi13;
+    RHO(iw31,iw32,iw13)=RHO(iw31,iw32,iw13)+wi31*wi32*wi13;
+
+    RHO(iw11,iw12,iw23)=RHO(iw11,iw12,iw23)+wi11*wi12*wi23;
+    RHO(iw21,iw12,iw23)=RHO(iw21,iw12,iw23)+wi21*wi12*wi23;
+    RHO(iw31,iw12,iw23)=RHO(iw31,iw12,iw23)+wi31*wi12*wi23;
+    RHO(iw11,iw22,iw23)=RHO(iw11,iw22,iw23)+wi11*wi22*wi23;
+    RHO(iw21,iw22,iw23)=RHO(iw21,iw22,iw23)+wi21*wi22*wi23;
+    RHO(iw31,iw22,iw23)=RHO(iw31,iw22,iw23)+wi31*wi22*wi23;
+    RHO(iw11,iw32,iw23)=RHO(iw11,iw32,iw23)+wi11*wi32*wi23;
+    RHO(iw21,iw32,iw23)=RHO(iw21,iw32,iw23)+wi21*wi32*wi23;
+    RHO(iw31,iw32,iw23)=RHO(iw31,iw32,iw23)+wi31*wi32*wi23;
+
+    RHO(iw11,iw12,iw33)=RHO(iw11,iw12,iw33)+wi11*wi12*wi33;
+    RHO(iw21,iw12,iw33)=RHO(iw21,iw12,iw33)+wi21*wi12*wi33;
+    RHO(iw31,iw12,iw33)=RHO(iw31,iw12,iw33)+wi31*wi12*wi33;
+    RHO(iw11,iw22,iw33)=RHO(iw11,iw22,iw33)+wi11*wi22*wi33;
+    RHO(iw21,iw22,iw33)=RHO(iw21,iw22,iw33)+wi21*wi22*wi33;
+    RHO(iw31,iw22,iw33)=RHO(iw31,iw22,iw33)+wi31*wi22*wi33;
+    RHO(iw11,iw32,iw33)=RHO(iw11,iw32,iw33)+wi11*wi32*wi33;
+    RHO(iw21,iw32,iw33)=RHO(iw21,iw32,iw33)+wi21*wi32*wi33;
+    RHO(iw31,iw32,iw33)=RHO(iw31,iw32,iw33)+wi31*wi32*wi33;
+  }
+}
+
 
 
 int main(int argc, char **argv)
@@ -118,65 +232,102 @@ int main(int argc, char **argv)
   FILE *fout;
   Particle *ptcl;
   char filename[256];
-  int64_t total_selected = 0;
+  double *mesh_density;
+  //int64_t total_selected = 0;
 
   if (argc != 2) {
     fprintf(stderr, "Usage: %s base_filename\n", argv[0]);
     return 1;
   }
 
-  fout = fopen("zslice_xy.dat", "w");
-  if (fout == NULL) {
+  /*fout = fopen("zslice_xy.dat", "w");
+    if (fout == NULL) {
     fprintf(stderr, "cannot open zslice_xy.dat\n");
     return 1;
-  }
+    }
 
-  int64_t npart_total = 0;
 
-  for (int32_t i = 0; i < 200; i++) {
+    int64_t npart_total = 0;
+
+    for (int32_t i = 0; i < 200; i++) {
     int32_t npart;
 
     fprintf(stdout, "# Reading %d-th file out of 200.\n", i);
 
     sprintf(filename, "%s.%d", argv[1], i);
+  */
 
-    npart = get_gadget_npart(filename);
+  int32_t npart;
+  npart = get_gadget_npart(argv[1]/*filename*/);
 
+  /*
     npart_total += npart;
     if (npart < 0) break;
+  */
 
-    ptcl = (Particle *) malloc(sizeof(Particle)*npart);
-    if (ptcl == NULL) {
-      fprintf(stderr, "malloc failed for %s\n", filename);
-      fclose(fout);
-      return 1;
-    }
+  ptcl = (Particle *) malloc(sizeof(Particle)*npart);
+  if (ptcl == NULL) {
+    fprintf(stderr, "malloc failed for %s\n", filename);
+    return 1;
+  }
 
-    read_gadget_ptcl(filename, ptcl);
+  read_gadget_ptcl(argv[1]/*filename*/, ptcl);
 
+  /*
     int32_t nsel = 0;
-#pragma omp parallel for schedule(auto) reduction(+:nsel)
+    #pragma omp parallel for schedule(auto) reduction(+:nsel)
     for (int32_t j = 0; j < npart; j++) {
-      if (ptcl[j].r[2] >= ZMIN && ptcl[j].r[2] <= ZMAX) {
-        fprintf(fout, "%14.6e %14.6e %14.6e\n",
-                ptcl[j].r[0],
-                ptcl[j].r[1],
-                ptcl[j].r[2]);
-        nsel++;
-      }
+    if (ptcl[j].r[2] >= ZMIN && ptcl[j].r[2] <= ZMAX) {
+    fprintf(fout, "%14.6e %14.6e %14.6e\n",
+    ptcl[j].r[0],
+    ptcl[j].r[1],
+    ptcl[j].r[2]);
+    nsel++;
+    }
     }
 
     total_selected += nsel;
     printf("#  selected in %12.4e <= z <= %12.4e : %d\n", ZMIN, ZMAX, nsel);
+  */
 
-    free(ptcl);
+  int datasize = NGRID * NGRID * NGRID;
+  mesh_density = (double *)malloc(sizeof(double) * datasize);
+  if (mesh_density == NULL) {
+    fprintf(stderr, "malloc failed for mesh_density\n");
+    fclose(fout);
+    return 1;
   }
 
+  calc_mesh_density(mesh_density, ptcl, npart, NGRID, BOXSIZE);
+
+  fout = fopen("density_tsc.dat", "w");
+  if (fout == NULL) {
+    fprintf(stderr, "cannot open density_tsc.dat\n");
+    free(mesh_density);
+    free(ptcl);
+    return 1;
+  }
+
+  for (int i = 0; i < NGRID; i++) {
+    for (int j = 0; j < NGRID; j++) {
+      for (int k = 0; k < NGRID; k++) {
+        fprintf(fout, "%d %d %d %14.6e\n",
+                i, j, k,
+                mesh_density[k + NGRID * (j + NGRID * i)]);
+      }
+    }
+  }
+
+  free(ptcl);
+  free(mesh_density);
   fclose(fout);
 
-  printf("# total number of particle in this snapshot : %ld\n", npart_total);
-  printf("# total selected particles = %lld\n", total_selected);
-  printf("wrote zslice_xy.dat\n");
+  printf("npart = %d\n", npart);
+  printf("wrote density_tsc.dat\n");
+
+  //printf("# total number of particle in this snapshot : %ld\n", npart_total);
+  //printf("# total selected particles = %lld\n", total_selected);
+  //printf("wrote zslice_xy.dat\n");
 
   return 0;
 }
