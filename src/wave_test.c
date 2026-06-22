ede2483d7a97dbb5a5f374c7e2b04446c801b54f
Kato0066 <amiami200034@gmail.com>
2026-06-22 13:50:58 +0900

    new

--- src/wave_test.c
+++ src/wave_test.c
@@ -2,6 +2,7 @@
 #include <math.h>
 
 #define N (128)
+#define OUTPUT_INTERVAL (10)
 
 int main(int argc, char **arg){
   int i, j, k;
@@ -10,9 +11,10 @@ int main(int argc, char **arg){
   double dx = 1.0 / (double)N;
   double dt = nu_cfl*dx/c;
   double PI = 3.141592653589793;
-
-  double tend;
-  int istep=0;
+  double tnow = 0.0;
+  double tend = 0.5;
+  int istep = 0;
+  int kmid = N / 2;
 
   static double U0[N][N][N];
   static double U1[N][N][N];
@@ -24,11 +26,11 @@ int main(int argc, char **arg){
   for(i=0;i<N;i++){
     for(j=0;j<N;j++){
       for(k=0;k<N;k++){
-	double x = (i+0.5)*dx;
-	double y = (j+0.5)*dx;
-	double z = (k+0.5)*dx;
-	U0[i][j][k]= exp(-100.0*((x-0.5)*(x-0.5)+(y-0.5)*(y-0.5)+(z-0.5)*(z-0.5)));
-	//U0[i][j]= sin(2.0*PI*x);
+        double x = (i+0.5)*dx;
+        double y = (j+0.5)*dx;
+        double z = (k+0.5)*dx;
+        U0[i][j][k]= exp(-100.0*((x-0.5)*(x-0.5)+(y-0.5)*(y-0.5)+(z-0.5)*(z-0.5)));
+        //U0[i][j]= sin(2.0*PI*x);
       }
     }
   }
@@ -56,34 +58,71 @@ int main(int argc, char **arg){
     }
   }
 
+#if 0
   printf("tendの値を入力してください。\n");
   scanf("%lf", &tend);
+#endif
+
+
+  FILE *output_wave_slice;
+  output_wave_slice = fopen("wave_slice_final.dat", "w");
 
   while(tnow < tend) {
     for(i=0;i<N;i++){
       for(j=0;j<N;j++){
-	for(k=0;k<N;k++){
-	  int ip = i+1 > N-1 ? i+1-N : i+1;
-	  int im = i-1 < 0 ? i-1+N : i-1;
-	  int jp = j+1 > N-1 ? j+1-N : j+1;
-	  int jm = j-1 < 0 ? j-1+N : j-1;
-	  int kp = k+1 > N-1 ? k+1-N : k+1;
-	  int km = k-1 < 0 ? k-1+N : k-1;
+        for(k=0;k<N;k++){
+          int ip = i+1 > N-1 ? i+1-N : i+1;
+          int im = i-1 < 0 ? i-1+N : i-1;
+          int jp = j+1 > N-1 ? j+1-N : j+1;
+          int jm = j-1 < 0 ? j-1+N : j-1;
+          int kp = k+1 > N-1 ? k+1-N : k+1;
+          int km = k-1 < 0 ? k-1+N : k-1;
 
-	  U2[i][j][k] =2.0 * U1[i][j][k] - U0[i][j][k] + A[i][j][k] * (U1[ip][j][k] + U1[im][j][k] + U1[i][jp][k] + U1[i][jm][k] + U1[i][j][kp] + U1[i][j][km] - 6.0 * U1[i][j][k]);
-	}
+          U2[i][j][k] =2.0 * U1[i][j][k] - U0[i][j][k] + A[i][j][k] * (U1[ip][j][k] + U1[im][j][k] + U1[i][jp][k] + U1[i][jm][k] + U1[i][j][kp] + U1[i][j][km] - 6.0 * U1[i][j][k]);
+        }
       }
     }
 
     for(i=0;i<N;i++){
       for(j=0;j<N;j++){
-	for(k=0;k<N;k++){
-	  U0[i][j][k] = U1[i][j][k];
-	  U1[i][j][k] = U2[i][j][k];
-	}
+        for(k=0;k<N;k++){
+          U0[i][j][k] = U1[i][j][k];
+          U1[i][j][k] = U2[i][j][k];
+        }
       }
     }
 
     tnow += dt;
     istep += 1;
+
+    if(istep%5==0) {
+      FILE *fp;
+      char name[120];
+
+      sprintf(name, "wave_slice-%03d.dat", istep);
+      fp_phi = fopen(name, "w");
+      for(i=0;i<N;i++) {
+        for(j=0;j<N;j++) {
+          fprintf(fp_phi, "%12.4e %12.4e %12.4e\n",
+                  dx*(double)i, dx*(double)j, U2[i][j][kmid]);
+        }
+        fprintf(fp, "\n");
+      }
+      fclose(fp);
+    }
   }
+
+  for(i=0;i<N;i++) {
+    for(j=0;j<N;j++) {
+      fprintf(output_wave_slice, "%12.4e %12.4e %12.4e\n",
+              dx*(double)i, dx*(double)j, U2[i][j][kmid]);
+    }
+    fprintf(output_wave_slice, "\n");
+  }
+  fprintf(output_wave_slice, "\n");
+
+  fclose(output_wave_slice);
+
+  return 0;
+
+}
